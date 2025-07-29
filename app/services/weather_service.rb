826a1abb1215a807301e2d37c0bba23f59588f76
yash-vaddoriya-2411 <trainee4.logicalstreet@gmail.com>
2025-07-29 15:08:02 +0530

    feat(weather): change weather api for accurate results

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -9,20 +9,25 @@ class WeatherService
 
   def fetch
     # api call to get weather data
-    response = self.class.get("/weather", query: {
+    response = self.class.get("/forecast", query: {
       lat: @lat,
       lon: @lon,
+      units: "metric",
       appid: Rails.application.credentials.dig(:weather_api),
-      units: "metric"
     })
 
     data = response.parsed_response
+
+    # Pick the first forecast (next 3-hour block)
+    first_entry = data["list"]&.first
+
+    return nil unless first_entry
     # return required api data
     {
-      curr_temp: data["main"]["temp"],
-      temp_min: data["main"]["temp_min"],
-      temp_max: data["main"]["temp_max"],
-      description: data["weather"].first["description"]
+      curr_temp: first_entry["main"]["temp"],
+      temp_min: first_entry["main"]["temp_min"],
+      temp_max: first_entry["main"]["temp_max"],
+      description: first_entry["weather"].first["description"]
     }
   end
 end
